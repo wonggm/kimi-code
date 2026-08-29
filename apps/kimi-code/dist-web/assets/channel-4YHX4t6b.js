f1118c1a0902ed802202b760d8482be01b459b71
wonggm <wong.gm@sjtu.edu.cn>
2026-08-30 01:10:00 +0800

    fix(build): adapt fork features to the 0.39 upstream base

--- apps/kimi-code/dist-web/assets/channel-4YHX4t6b.js
+++
@@ -1,1 +0,0 @@
-import{U as a,O as n}from"./mermaid.core-Ci_ShHqV.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
--- apps/kimi-code/dist-web/assets/swimlanesDiagram-G3AALYLV-BFyz0Z8a.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U-BLiHy1va.js";import{_ as a}from"./mermaid.core-Ci_ShHqV.js";import"./chunk-5VM5RSS4-CC9yzw-p.js";import"./chunk-XXDRQBXY-gteaXiEE.js";import"./chunk-VR4S4FIN-BBaFYq-u.js";import"./chunk-32BRIVSS-BTh5nWiN.js";import"./channel-4YHX4t6b.js";import"./index-ClWTW3HX.js";import"./_commonjsHelpers-CqkleIqs.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
